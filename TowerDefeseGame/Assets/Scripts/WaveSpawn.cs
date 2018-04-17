fe42f536159ea2cf74cdb6c2451e6db3ee1e03b3
vunguyenbg <38451681+vunguyenbg@users.noreply.github.com>
2018-04-17 14:01:35 +0700

    Add

---
+++ TowerDefeseGame/Assets/Scripts/WaveSpawn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawn : MonoBehaviour {
+
+    public GameObject enemy;
+    public Text countDownText;
+    public Transform start;
+    private float countDown = 2;
+    private float MaxCountDown = 5;
+    private int waveNumber = 1;
+    private float timeDelay = 0f;
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        countDown -= Time.deltaTime;
+        countDownText.text = Mathf.Round(countDown).ToString();
+        if (countDown <= 0f)
+        {
+            countDown = MaxCountDown;
+            countDownText.text = countDown.ToString();
+            StartCoroutine(SpawnDelay());
+        }
+	}
+    void SpawnEnemy()
+    {
+        Instantiate(enemy, start.position, start.rotation);
+    }
+    IEnumerator SpawnDelay()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.4f);
+        }
+        waveNumber++;
+    }
+}
---
+++ TowerDefeseGame/Assets/Scripts/WayPoints.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoints : MonoBehaviour {
+
+    public static Transform[] point;
+	void Awake () 
+    {
+        point = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            point[i] = transform.GetChild(i).transform;
+        }
+	}
+}
